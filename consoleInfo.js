b51819f8158b592bd2cc1edf6f166ffdd7141ab3
Evert De Spiegeleer <evert.despiegeleer@ugent.be>
2019-07-17 10:42:58 +0200

    Update consoleInfo.js

--- consoleInfo.js
+++ consoleInfo.js
@@ -3,6 +3,7 @@ console.b2xk = console.info;
 if (typeof window === 'undefined') {
     console.info = function (msg) {
         console.b2xk("\u001B[44m\u001B[37m", "\u2139", "\u001B[0m", "\u001B[36m", msg, "\u001B[0m");
+        module.exports = console.info;
     };
 }
 else { //Running in the browser
@@ -10,4 +11,3 @@ else { //Running in the browser
         console.b2xk("%c[i]%c " + msg, "background: blue; color: #FFF", "color: blue");
     };
 }
-module.exports = console.info;
